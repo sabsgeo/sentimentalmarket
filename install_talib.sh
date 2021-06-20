85b29a2b3e6c840ee019296778132b32c2645066
sabsgeo <sabugeorge.mec@gmail.com>
2021-06-20 19:09:34 +0530

    Update install_talib.sh

--- install_talib.sh
+++ install_talib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 P=$(mktemp -d -t)
 wget -P $P https://github.com/sabsgeo/sentimentalmarket/raw/main/deps/ta-lib-0.4.0-src.tar.gz
-cd $P && tar -xzf ta-lib-0.4.0-src.tar.gz && cd ta-lib/ && ./configure --prefix=/usr && make && sudo make install
+cd $P && tar -xzf ta-lib-0.4.0-src.tar.gz && cd ta-lib/ && ./configure --prefix=/usr && make && make install
 # clean up
 rm -rf $P
