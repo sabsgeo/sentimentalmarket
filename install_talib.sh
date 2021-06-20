1f81729d386f4d0ee224f0f3038321119018bdf9
sabsgeo <sabugeorge.mec@gmail.com>
2021-06-20 14:45:52 +0000

    [WIP]:updated some doc

--- install_talib.sh
+++ install_talib.sh
@@ -4,3 +4,4 @@ wget -P $P https://github.com/sabsgeo/sentimentalmarket/raw/main/deps/ta-lib-0.4
 cd $P && tar -xzf ta-lib-0.4.0-src.tar.gz && cd ta-lib/ && ./configure --prefix=/usr && make && make install
 # clean up
 rm -rf $P
+# export PIP_USER=no was required for development in git pod for installing python package into virtual environment
