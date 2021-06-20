ed04ee760621d72c0ff4dd59560344b11bdbfcc4
sabsgeo <sabugeorge.mec@gmail.com>
2021-06-20 19:02:41 +0530

    Update install_talib.sh

--- install_talib.sh
+++ install_talib.sh
@@ -2,8 +2,5 @@
 P=$(mktemp -d -t)
 wget -P $P https://github.com/sabsgeo/crypto-trading-bot/raw/main/deps/ta-lib-0.4.0-src.tar.gz
 cd $P && tar -xzf ta-lib-0.4.0-src.tar.gz && cd ta-lib/ && ./configure --prefix=/usr && make && sudo make install
-export PIP_USER=no
-sudo apt-get update
-sudo apt-get install python3-dev -y
 # clean up
 rm -rf $P
